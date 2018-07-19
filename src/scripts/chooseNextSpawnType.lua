b6eca4f937dd4390d638c66b4a08da3d60e57d24
Anythingapplied <anythingapplied@userdelta.com>
2018-07-18 20:37:54 -0500

    finetuning spawn balance

--- src/scripts/chooseNextSpawnType.lua
+++ src/scripts/chooseNextSpawnType.lua
@@ -7,11 +7,11 @@
 
 building_list = {"buffer", "big-furnace", "big-assembly", "big-refinery"}
 -- The ideal propotions of different kinds of machine for end game
-local goal_proportion = {["buffer"] = 30, ["big-furnace"] = 35, ["big-assembly"] = 35, ["big-refinery"] = 2}
+local goal_proportion = {["buffer"] = 35, ["big-furnace"] = 35, ["big-assembly"] = 35, ["big-refinery"] = 2}
 
 -- Offsets to trick the game into thinking your starting amounts of machines are at certain levels.
 -- Higher numbers will push spawning off until more factories are discovered, negative numbers will spawn more earlier
-local initial_boost = {["buffer"] = 0, ["big-furnace"] = 0, ["big-assembly"] = -30, ["big-refinery"] = 1}
+local initial_boost = {["buffer"] = 0, ["big-furnace"] = 0, ["big-assembly"] = -30, ["big-refinery"] = 0}
 
 -- Calculate sums of all sets of points for probability calculation
 local total_proportion = 0
